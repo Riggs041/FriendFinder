e441bcbe1d960b05ddd42f4d17c51aa724267f80
riggs <riggs041@gmail.com>
2018-07-24 00:22:11 -0500

    updated files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,53 @@
 var newCharacter = [
     {
         name: "Michael Scott",
-        photo: "https://i2.wp.com/mtncatholic.com/wp-content/uploads/2016/10/6357600113572837231773916132_michael-scott-s-top-tantrums.png?fit=500%2C359",
-        scores: [5, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest/scale-to-width-down/2000?cb=20170701090332",
+        scores: [5, 4, 2, 3, 4, 2, 3, 1, 3, 1]
     },
     {
         name: "Jim Halpert",
-        photo: "",
-        scores: [1, 1, 1, 1, 1, 1, 5, 1, 1, 1]
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg/revision/latest?cb=20170701084550",
+        scores: [4, 4, 1, 1, 2, 1, 5, 1, 4, 1]
     },
     {
         name: "Dwight Schrute",
-        photo: "",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 5]
+        photo: "http://community.digitalmediaacademy.org/wp-content/uploads/2011/05/The-Office.jpg",
+        scores: [4, 1, 4, 1, 1, 4, 1, 1, 1, 5]
     },
     {
         name: "Pam Beesly",
-        photo: "",
-        scores: [1, 1, 5, 1, 1, 1, 1, 1, 1, 1]
+        photo: "https://nationalpostcom.files.wordpress.com/2011/11/theofficepam.gif",
+        scores: [1, 3, 5, 1, 3, 3, 3, 2, 3, 3]
     },
     {
         name: "Kelly Kapoor",
-        photo: "",
-        scores: [1, 1, 1, 1, 5, 1, 1, 1, 1, 1]
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/6/69/Kelly_Kapoor.jpg/revision/latest?cb=20170701090501",
+        scores: [2, 5, 3, 4, 5, 2, 1, 4, 4, 1]
     },
     {
         name: "Andy Bernard",
-        photo: "",
-        scores: [1, 1, 1, 5, 1, 1, 1, 1, 1, 1]
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/b/b5/Andy_Bernard.jpg/revision/latest?cb=20170701084205",
+        scores: [4, 3, 4, 5, 4, 4, 3, 2, 5, 1]
     },
     {
         name: "Kevin Malone",
-        photo: "",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 5, 1]
+        photo: "https://static.tvgcdn.net/rovi/showcards/feed/214/thumbs/30656214_1300x1733.jpg",
+        scores: [1, 4, 4, 3, 1, 1, 1, 1, 5, 1]
     },
     {
         name: "Toby Flenderson",
-        photo: "",
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/5/55/2009TobyCropped.PNG/revision/latest/scale-to-width-down/350?cb=20170701082913",
         scores: [1, 1, 1, 1, 1, 1, 1, 5, 1, 1]
     },
     {
         name: "Angela Martin",
-        photo: "",
-        scores: [1, 1, 1, 1, 1, 5, 1, 1, 1, 1]
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/Angela_Martin.jpg/230px-Angela_Martin.jpg",
+        scores: [1, 1, 1, 1, 1, 5, 1, 1, 1, 5]
     },
     {
         name: "Ryan Howard",
-        photo: "",
-        scores: [1, 5, 1, 1, 1, 1, 1, 1, 1, 1]
+        photo: "https://upload.wikimedia.org/wikipedia/en/9/91/Ryan_Howard_%28The_Office%29.jpg",
+        scores: [5, 5, 5, 5, 1, 5, 5, 5, 5, 1]
     },
 
 ];
